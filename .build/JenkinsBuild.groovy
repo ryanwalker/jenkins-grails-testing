46840194a8f0b5b2571bd74bae9f5c5f6338cc69
Ryan Walker <ryan@infusionsoft.com>
2014-09-26 13:16:18 -0700

    Added Junit Test Reporting to build

--- .build/JenkinsBuild.groovy
+++ .build/JenkinsBuild.groovy
@@ -14,41 +14,49 @@ job {
         }
     }
 
+//    triggers {
+//        scm('*/5 * * * *')
+//    }
+
     steps {
         grails {
             target("test-app")
             name("Grails 2.3.11")
 
         }
-    }
 
-}
-
-job {
-    name('ryans-grails-war-job')
-    displayName('Ryans Grails War Job')
-
-    scm {
-        git {
-            remote {
-                url('https://github.com/ryanwalker/jenkins-grails-testing.git')
-            }
-            branch('master')
-            createTag(false)
-            clean(true)
-
+        publishers {
+            archiveJunit 'target/test-reports/**/*Spec.xml'
+//            archiveArtifacts 'target/**/*.war'
         }
     }
 
-    triggers {
-        scm('H/5 * * * *')
-    }
+    chucknorris()
 
-    steps {
-        grails {
-            target("war")
-            name("Grails 2.3.11")
-        }
-    }
 
 }
+
+//job {
+//    name('ryans-grails-war-job')
+//    displayName('Ryans Grails War Job')
+//
+//    scm {
+//        git {
+//            remote {
+//                url('https://github.com/ryanwalker/jenkins-grails-testing.git')
+//            }
+//            branch('master')
+//            createTag(false)
+//            clean(true)
+//
+//        }
+//    }
+//
+//    steps {
+//        grails {
+//            target("war")
+//            name("Grails 2.3.11")
+//        }
+//    }
+//
+//}
